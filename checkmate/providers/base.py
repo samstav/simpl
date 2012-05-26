b3a892e2c5d52a9888ca074e2780512ae108e7d6
Ziad Sawalha <github@highbridgellc.com>
2012-05-26 15:08:17 -0500

    Update Provider Base
    - prep for getting loadbalancer and database in

--- checkmate/providers/base.py
+++ checkmate/providers/base.py
@@ -10,13 +10,26 @@ class ProviderBase():
         self.dict = provider
 
     def provides(self):
+        """Returns  a list of resources that this provider can provder"""
         return self.dict.get('provides', [])
 
     def generate_template(self, deployment, service_name, service, name=None):
+        """Generate a resource dict to be embedded ina  deployment"""
+        raise NotImplementedError()
+
+    def prep_environment(self, wfspec, deployment):
+        """Asdd any tasks that are needed for an environment setup"""
+        raise NotImplementedError()
+
+    def add_resource_tasks(self, resource, key, wfspec, deployment,
+            stockton_deployment, wait_on=None):
+        """Add tasks needed to create a resource (the resource would normally
+            what was generated in the generate_template call)"""
         raise NotImplementedError()
 
 
 def get_provider_class(vendor, key):
+    """Given a vendor name, and provider kjey, returjn the provider class"""
     name = "%s.%s" % (vendor, key)
     class_name = "checkmate.providers.%s" % name.replace('-', '_')
     LOG.debug("Instantiating provider class: %s" % class_name)
