be5fbdd64669a164f821e8f0a3b674b64fde5ccd
Ziad Sawalha <github@highbridgellc.com>
2012-05-28 02:29:22 -0500

    Add Base Provider Methods
    - So providers can be called to prep an
      environment for deployment
    - Providers will be called to add workflow tasks
      for their resources

--- checkmate/providers/base.py
+++ checkmate/providers/base.py
@@ -14,18 +14,24 @@ class ProviderBase():
         return self.dict.get('provides', [])
 
     def generate_template(self, deployment, service_name, service, name=None):
-        """Generate a resource dict to be embedded ina  deployment"""
-        raise NotImplementedError()
+        """Generate a resource dict to be embedded in a  deployment"""
+        pass
 
     def prep_environment(self, wfspec, deployment):
-        """Asdd any tasks that are needed for an environment setup"""
-        raise NotImplementedError()
+        """Add any tasks that are needed for an environment setup
+        :returns: the final task that signifies readiness"""
+        pass
 
     def add_resource_tasks(self, resource, key, wfspec, deployment,
             context, wait_on=None):
         """Add tasks needed to create a resource (the resource would normally
-            what was generated in the generate_template call)"""
-        raise NotImplementedError()
+            what was generated in the generate_template call)
+        :param wait_on: tasks to wait on before executing
+        :returns: a hash (dict) of relevant tasks. The hash keys are:
+                'root': the root task in the sequence
+                'final': the task that signifies readiness (work is done)
+        """
+        pass
 
 
 def get_provider_class(vendor, key):
