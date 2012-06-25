b135e7f7a47bfe88929ab1b3eb8d23fd704db6c3
Jarret Raim <jarret.raim@rackspace.com>
2012-06-25 08:48:10 -0500

    Auth & UI Improvements
    Auth doesn't currently work due to same origin.

--- checkmate/static/angular/js/app.js
+++ checkmate/static/angular/js/app.js
@@ -3,6 +3,7 @@ angular.module('checkmate', ['checkmateFilters', 'checkmateServices', 'ngSanitiz
     $routeProvider.
         when('/environments', {templateUrl: 'partials/environment-list.html',   controller: EnvironmentListCtrl}).
         when('/environments/:environmentId', {templateUrl: 'partials/environment-detail.html', controller: EnvironmentDetailCtrl}).
+        when('/profile', {templateUrl: 'partials/profile.html', controller: ProfileCtrl}).
         otherwise({redirectTo: '/'});
   }])
   .config(['$locationProvider', function($locationProvider) {
--- checkmate/static/angular/js/controllers.js
+++ checkmate/static/angular/js/controllers.js
@@ -11,6 +11,10 @@ function EnvironmentListCtrl($scope, $location, Environment) {
   $scope.create = function() {
     $location.path('/environments/new');
   }
+
+  $scope.navigate = function(environmentId) {
+    $location.path('/environments/' + environmentId);
+  }
 }
 EnvironmentListCtrl.$inject = ['$scope', '$location', 'Environment']; 
 
@@ -43,12 +47,57 @@ function AuthCtrl($scope, $location) {
     catalog: null
   };
 
+  var modal = $('#auth_modal');
+  modal.modal({
+    keyboard: false,
+    show: true
+  });
+
+  $('#auth_modal').modal('show');
+
   $scope.authenticated = function() {
     return $scope.auth.catalog != null;
   }
 
-  $scope.authenticate = function() {
+  $scope.signOut = function() {
+    $scope.auth = {
+      username: '',
+      password: '',
+      catalog: null
+    };
+    $location('/');
   }
 
+  $scope.authenticate = function() {
+    $('#auth_loader').show();
+
+    return $.ajax({
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      async: false,
+      url: "https://identity.api.rackspacecloud.com/v1.1/auth",
+      data: JSON.stringify({
+              "credentials": {
+                "username": $scope.auth.username,
+                "key": $scope.auth.password
+              }
+            }),
+    }).always(function(json) {
+      $scope.auth.catalog = json;
+    }).success(function() {
+      $('#auth_modal').modal('hide');
+      $('#auth_loader').hide();
+    }).error(function() {
+      $("#auth_error_text").html("Something bad happened");
+      $('#auth_loader').hide();
+      $("#auth_error").show();
+    });
+  }
 }
 AuthCtrl.$inject = ['$scope', '$location']
+
+function ProfileCtrl($scope, $location) {
+
+}
+ProfileCtrl.$inject = ['$scope', '$location'];
