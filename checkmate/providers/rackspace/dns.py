666eda097fd0943b0083e5736e87cd2969458fce
Jessica Lucci <jessicalucci14@gmail.com>
2012-08-16 08:50:31 -0500

    Fixed formatting to pass pep8 tests

--- checkmate/providers/rackspace/dns.py
+++ checkmate/providers/rackspace/dns.py
@@ -13,19 +13,18 @@ class Provider(ProviderBase):
     vendor = 'rackspace'
 
     def add_resource_tasks(self, resource, key, wfspec, deployment, context,
-                wait_on=None):
+                           wait_on=None):
         inputs = deployment.get('inputs', {})
         hostname = resource.get('dns-name')
 
         create_dns_task = Celery(wfspec, 'Create DNS Record',
-                           'checkmate.providers.rackspace.dns.create_record',
-                           call_args=[Attrib('context'),
-                           inputs.get('domain', 'localhost'), hostname,
-                           'A', Attrib('vip')],
-                           defines=dict(resource=key,
-                                        provider=self.key,
-                                        task_tags=['final', 'root', 'create']),
-                           properties={'estimated_duration': 30})
+                                 'checkmate.providers.rackspace.dns.create_record',
+                                 call_args=[Attrib('context'),
+                                 inputs.get('domain', 'localhost'), hostname,
+                                 'A', Attrib('vip')],
+                                 defines=dict(resource=key, provider=self.key,
+                                 task_tags=['final', 'root', 'create']),
+                                 properties={'estimated_duration': 30})
         return dict(root=create_dns_task, final=create_dns_task)
 
     def get_catalog(self, context, type_filter=None):
@@ -68,22 +67,22 @@ def parse_domain(domain_str):
 
 """ Celery tasks """
 
+
 @task(default_retry_delay=10, max_retries=10)
-def get_domains(deployment,limit=None,offset=None):
+def get_domains(deployment, limit=None, offset=None):
     api = _get_dns_object(deployment)
     try:
-	DomainResults = api.get_domains(limit=limit,offset=offset)
-	return DomainResults._domains
-	LOG.debug('Successfully retreived domains.')
+        DomainResults = api.get_domains(limit=limit, offset=offset)
+        LOG.debug('Successfully retreived domains.')
+        return DomainResults._domains
     except Exception, exc:
-	LOG.debug('Error retreiving domains. Error: %s. Retrying.' % exc)
-	get_domains.retry(exc=exc)
-
+        LOG.debug('Error retreiving domains. Error: %s. Retrying.' % exc)
+        get_domains.retry(exc=exc)
 
 
 @task(default_retry_delay=10, max_retries=10)
 def create_domain(deployment, domain,
-                             email='soa_placeholder@example.com', ttl=300):
+                  email='soa_placeholder@example.com', ttl=300):
     api = _get_dns_object(deployment)
     try:
         api.create_domain(name=domain, ttl=ttl, emailAddress=email)
@@ -97,7 +96,7 @@ def create_domain(deployment, domain,
         create_domain.retry(exc=r)
     except Exception, exc:
         LOG.debug('Unknown error creating domain %s. Error: %s. Retrying.' % (
-                domain, str(exc)))
+                  domain, str(exc)))
         create_domain.retry(exc=exc)
 
 
@@ -108,11 +107,11 @@ def delete_domain(deployment, name):
         domain = api.get_domain(name=name)
     except UnknownDomain, exc:
         LOG.debug('Cannot deleted domain %s because it does not exist. ' \
-                'Refusing to retry.' % name)
+                  'Refusing to retry.' % name)
         return
     except Exception, exc:
         LOG.debug('Exception getting domain %s. Was hoping to delete it. ' \
-                'Error %s. Retrying.' % (name, str(exc)))
+                  'Error %s. Retrying.' % (name, str(exc)))
         delete_domain.retry(exc=exc)
 
     try:
@@ -120,50 +119,51 @@ def delete_domain(deployment, name):
         LOG.debug('Domain %s deleted.' % name)
     except ResponseError, r:
         LOG.debug('Error deleting domain %s (%s) %s. Retrying.' % (
-            name, r.status, r.reason))
+                  name, r.status, r.reason))
         delete_domain.retry(exc=r)
     except Exception, exc:
         LOG.debug('Error deleting domain %s. Error %s. Retrying.' % (
-                name, str(exc)))
+                  name, str(exc)))
         delete_domain.retry(exc=exc)
 
 
 @task(default_retry_delay=20, max_retries=10)
 def create_record(deployment, domain, name, dnstype, data,
-                             ttl=1800, makedomain=False,
-                             email='soa_placeholder@example.com'):
+                  ttl=1800, makedomain=False,
+                  email='soa_placeholder@example.com'):
     api = _get_dns_object(deployment)
     try:
         domain_object = api.get_domain(name=domain)
     except UnknownDomain, exc:
         if makedomain:
             LOG.debug('Cannot create %s record (%s->%s) because %s does not ' \
-                    'exist. Creating %s and retrying.' % (
-                    dnstype, name, data, domain, domain))
+                      'exist. Creating %s and retrying.' % (
+                      dnstype, name, data, domain, domain))
             create_domain.delay(deployment, domain, email=email)
             create_record.retry(exc=exc)
         else:
             LOG.debug('Cannot create %s record (%s->%s) because %s does not ' \
-                'exist. Refusing to retry.' % (dnstype, name, data, domain))
+                      'exist. Refusing to retry.' % (
+                      dnstype, name, data, domain))
             return
     except Exception, exc:
         LOG.debug('Error finding domain %s.  Wanting to create %s record (%s' \
-            '->%s TTL: %s). Error %s. Retrying.' % (
-              domain, dnstype, name, data, ttl, str(exc)))
+                  '->%s TTL: %s). Error %s. Retrying.' % (
+                  domain, dnstype, name, data, ttl, str(exc)))
         create_record.retry(exc=exc)
 
     try:
         domain_object.create_record(name, data, dnstype, ttl=ttl)
         LOG.debug('Created DNS %s record %s -> %s. TTL: %s' % (
-            dnstype, name, data, ttl))
+                  dnstype, name, data, ttl))
     except ResponseError, r:
         LOG.debug('Error creating DNS %s record %s -> %s. TTL: %s Error: %s ' \
-            '%s. Retrying.' % (
-              dnstype, name, data, ttl, r.status, r.reason))
+                  '%s. Retrying.' % (
+                  dnstype, name, data, ttl, r.status, r.reason))
         create_record.retry(exc=r)
     except Exception, exc:
         LOG.debug('Error creating DNS %s record %s -> %s. TTL: %s Error: %s.' \
-            ' Retrying.' % (dnstype, name, data, ttl, str(exc)))
+                  ' Retrying.' % (dnstype, name, data, ttl, str(exc)))
         create_record.retry(exc=exc)
 
 
@@ -174,11 +174,11 @@ def delete_record(deployment, domain, name):
         domain = api.get_domain(name=domain)
     except UnknownDomain, exc:
         LOG.debug('Cannot delete record %s because %s does not exist. ' \
-            'Refusing to retry.' % (name, domain))
+                  'Refusing to retry.' % (name, domain))
         return
     except Exception, exc:
         LOG.debug('Error finding domain %s.  Wanting to delete record %s. ' \
-            'Error %s. Retrying.' % (domain, name, str(exc)))
+                  'Error %s. Retrying.' % (domain, name, str(exc)))
         delete_record.retry(exc=exc)
 
     try:
@@ -187,15 +187,9 @@ def delete_record(deployment, domain, name):
         LOG.debug('Deleted DNS record %s.' % name)
     except ResponseError, r:
         LOG.debug('Error deleting DNS record %. Error %s %s. Retrying.' % (
-            name, r.status, r.reason))
+                  name, r.status, r.reason))
         delete_record.retry(exc=r)
     except Exception, exc:
         LOG.debug('Error deleting DNS record %s. Error %s. Retrying.' % (
-            name, str(exc)))
+                  name, str(exc)))
         delete_record.retry(exc=exc)
-
-
-
-
-
-
