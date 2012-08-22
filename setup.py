3f345be15337faa633360ffc4997d89baf2d856c
Craig Cobb <craig.cobb@rackspace.com>
2012-08-22 14:00:36 -0500

    Fixed syntax problems, further resolved requirement versions

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@ from setuptools import setup, find_packages
 # Provide URLs to Github projects if they're not pip-aware
 gh = 'https://github.com'
 github_projects = [{'project': 'python-clouddns', 'user': 'rackspace'},
-                   {'project': 'python-clouddb', 'user': 'slizadel'},
-                   {'project': 'openstack.compute', 'user': 'jacobian'},
+                   {'project': 'python-clouddb', 'user': 'slizadel', 'version': '-.01'},
+                   {'project': 'openstack.compute', 'user': 'jacobian', 'version': '-2.0a1'},
                    {'project': 'python-keystoneclient', 'user': 'openstack'},
                    {'project': 'python-novaclient', 'user': 'openstack'},
-                   {'project': 'pychef', 'user': 'calebgroom'},
+                   {'project': 'pychef', 'user': 'calebgroom', 'version': '-0.2.dev'},
                    {'project': 'rackspace-monitoring', 'user': 'racker'},
-                   {'project': 'SpiffWorkflow', 'user': 'ziadsawalha',
-                      'branch': 'celery'}]
+                   {'project': 'SpiffWorkflow', 'user': 'ziadsawalha','branch': 'celery', 'version': '-0.3.2-rackspace'}]
 
 github_urls = []
 for p in github_projects:
-    github_urls.append('https://github.com/%s/%s/tarball/%s#egg=%s' % (
+    github_urls.append('https://github.com/%s/%s/tarball/%s#egg=%s%s' % (
                        p['user'], p['project'], p.get('branch', 'master'),
-                       p['project']))
+                       p['project'], p.get('version', '')))
 
 setup(
     name='checkmate',
@@ -30,24 +29,25 @@ setup(
     install_requires=['bottle==0.10.11',
                       'celery==2.5.5',
                       'kombu==2.1.8',
-                      'GitPython==0.3.2.RC1'
-                      'Jinja2==2.6'
+                      'GitPython==0.3.2.RC1',
+                      'Jinja2==2.6',
                       'openstack.compute==2.0a1',
-                      'pam==0.1.4'
-                      'paramiko==1.7.7.2'
-                      'pycrypto==2.6'
+                      'pam==0.1.4',
+                      'paramiko==1.7.7.2',
+                      'pycrypto==2.6',
                       'python-clouddb==.01',
-                      'python-novaclient==2012.2',
-                      'python-cloudlb==0.5.1'
-                      'python-keystoneclient==0.0.0',
-                      'python-clouddns==0.0.0',
-                      'python-cloudfiles==1.7.10'
+                      'python-novaclient', #==2012.2
+                      'python-cloudlb==0.5.1',
+                      'python-keystoneclient',
+                      'python-clouddns',
+                      'python-cloudfiles==1.7.10',
+                      'rackspace-monitoring',
                       'PyChef==0.2.dev',
-                      'PyYAML==3.10'
+                      'PyYAML==3.10',
                       'SpiffWorkflow==0.3.2-rackspace',
-                      'SQLAlchemy==0.7.8'
-                      'sqlalchemy-migrate==0.7.2'
-                      'WebOb==1.2.2'
+                      'SQLAlchemy==0.7.8',
+                      'sqlalchemy-migrate==0.7.2',
+                      'WebOb==1.2.2',
                       'prettytable==0.6',
                       'pymongo==2.1.1',
                       ],
