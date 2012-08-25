d9f50991a0cd5e31800cd4ca5bf2ad384009a5c1
Ziad Sawalha <github@highbridgellc.com>
2012-08-25 01:14:04 -0500

    Provider: ssh - fix retry problem
    Retries were erring out with incorrect number of
    arguments. Not sure why, but this fixes it.

--- checkmate/ssh.py
+++ checkmate/ssh.py
@@ -46,9 +46,10 @@ def test_connection(deployment, ip, username, timeout=10, password=None,
         if isinstance(exc, paramiko.PasswordRequiredException):
             #Looks like we have cert issues, so try password auth if we can
             if password:
+                LOG.debug("Retrying with password credentials")
                 if test_connection(deployment, ip, username, timeout=timeout,
-                    password=password, identity_file=None, port=port,
-                    callback=callback):
+                        password=password, identity_file=None, port=port,
+                        callback=callback):
                     LOG.debug("Authentication for ssh://%s@%s:%d using "
                             "password succeeded" % (username, ip, port))
                     return True
@@ -56,23 +57,23 @@ def test_connection(deployment, ip, username, timeout=10, password=None,
         LOG.debug('Authentication for ssh://%s@%s:%d failed. Type: %s' %
                 (username, ip, port, auth_type))
         if test_connection.request.id:
-            test_connection.retry(exc=exc)
+            #TODO: figure out why retry without args/kwargs fails!
+            test_connection.retry(args=[deployment, ip, username], kwargs=dict(
+                    timeout=timeout, password=password, identity_file=None,
+                    port=port, callback=callback), exc=exc)
     except paramiko.BadHostKeyException, exc:
         msg = ("ssh://%s@%s:%d failed:  %s. You might have a bad key "
                 "entry on your server, but this is a security issue and won't "
                 "be handled automatically. To fix this you can remove the "
                 "host entry for this host from the /.ssh/known_hosts file" % (
                     username, ip, port, exc))
-        print msg
         LOG.debug(msg)
         if test_connection.request.id:
             test_connection.update_state(state='FAILURE', meta={'Message':
                     msg})
         raise exc
     except Exception, exc:
-        print exc
-        LOG.debug('ssh://%s@%s:%d failed.  %s' % (
-            username, ip, port, exc))
+        LOG.debug('ssh://%s@%s:%d failed.  %s' % (username, ip, port, exc))
         if test_connection.request.id:
             test_connection.retry(exc=exc)
     return False
