ef836a60978b457c4e1f49031a951c03b1591af8
Caleb Groom <cgroom@rackspace.com>
2012-08-31 10:33:08 -0500

    Vagrant: drop old templates.

--- vagrant/site-cookbooks/checkmate/attributes/default.rb
+++ vagrant/site-cookbooks/checkmate/attributes/default.rb
@@ -1,8 +1,9 @@
 default['checkmate']['path'] = "/opt/checkmate"
 default['checkmate']['queue_path'] = '/opt/checkmate/bin/checkmate-queue'
-default['checkmate']['data_path'] = "/opt/checkmate/data"
-default['checkmate']['connection_string'] = "sqlite:////opt/checkmate/data/db.sqlite"
-default['checkmate']['connection_string_results'] = "sqlite:////opt/checkmate/data/db.sqlite"
+default['checkmate']['datastore'] = 'sqlite'
+default['checkmate']['sqlite_path'] = '/opt/checkmate/data'
+default['checkmate']['connection_string'] = 'sqlite:////opt/checkmate/data/db.sqlite'
+default['checkmate']['connection_string_results'] = 'sqlite:////opt/checkmate/data/db.sqlite'
 default['checkmate']['domain'] = 'example.com'
 default['checkmate']['public_key'] = '~/.ssh/id_rsa.pub'
 default['checkmate']['chef_path'] = '/opt/checkmate/chef'
@@ -10,11 +11,11 @@ default['checkmate']['chef_local_path'] = '/opt/checkmate/chef/local'
 default['checkmate']['chef_repo'] = '/opt/checkmate/chef/repo/chef-stockton'
 default['checkmate']['use_data_bags'] = 'True'
 default['checkmate']['celery']['force_execv'] = 'True'
-default['checkmate']['broker']['username'] = "checkmate"
-default['checkmate']['broker']['password'] = "checkmate"
-default['checkmate']['broker']['port'] = "5672"
-default['checkmate']['broker']['host'] = "localhost"
-default['checkmate']['broker']['vhost'] = "checkmate"
+default['checkmate']['broker']['username'] = 'checkmate'
+default['checkmate']['broker']['password'] = 'checkmate'
+default['checkmate']['broker']['port'] = '5672'
+default['checkmate']['broker']['host'] = 'localhost'
+default['checkmate']['broker']['vhost'] = 'checkmate'
 default['checkmate']['broker']['url'] = sprintf("amqp://%s:%s@%s:%d/%s",
   node['checkmate']['broker']['username'],
   node['checkmate']['broker']['password'],
