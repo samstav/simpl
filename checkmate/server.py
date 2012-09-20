9a1239049de05f20bbe614ec75c238edc2aeaf37
ziad-sawalha <ziad.sawalha@rackspace.com>
2012-09-20 11:42:15 -0500

    Adding Tracing Capability
    Got python-tracer from
    https://github.com/ziadsawalha/Python-tracer
    
    Added -t/--trace-calls support to command-line

--- checkmate/server.py
+++ checkmate/server.py
@@ -6,6 +6,8 @@ import logging
 import string
 import sys
 
+import checkmate.common.tracer  # @UnusedImport # module runs on import
+
 # Init logging before we load the database, 3rd party, and 'noisy' modules
 from checkmate.utils import init_console_logging
 init_console_logging()
