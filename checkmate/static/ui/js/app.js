fb2d6e255bd59388d9b400bac9c0fdafad2bbe32
BK Box <bk.box@rackspace.com>
2012-10-10 11:41:28 -0500

    constraint for dbaas disk size (not implemented)

--- checkmate/static/ui/js/app.js
+++ checkmate/static/ui/js/app.js
@@ -1594,7 +1594,13 @@ WPBP = {
                 ],
                 "type":"int",
                 "description":"The hard drive space available for the database instance in GB.",
-                "label":"Database Disk Size"
+                "label":"Database Disk Size",
+                "constraints":[
+                    {
+                        "greater-than":0,
+                        "less-than":51
+                    }
+                ]
             },
             "ssl":{
                 "default":false,
