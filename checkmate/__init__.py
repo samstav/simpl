84fe6dab21764c60bb84ab8d7759c3eb210e5362
Craig Cobb <craig.cobb@rackspace.com>
2012-11-13 16:53:46 -0600

    Consolidating build-type to release value. Bumping to master version.

--- checkmate/__init__.py
+++ checkmate/__init__.py
@@ -9,33 +9,33 @@ import re
 # don't need to.
 gettext.install('checkmate')
 
+
 configfile = os.path.join(os.path.dirname(__file__), 'checkmate.cfg')
 config = ConfigParser()
 config.read(configfile)
 
 __version__ = config.get("checkmate", "version")
-__release__ = config.get("checkmate", "release")
-__build_type__ = None
+__release__ = None
 
 
-def get_build_type():
-    global __build_type__
-    if not __build_type__:
-        import pkg_resources
-        val = "unknown"
-        try:
-            dist = pkg_resources.get_distribution("checkmate")
-            match = re.search('((\d+\.)+)(\D.+)', dist.version)
-            if match:
-                val = match.group(match.lastindex)
-        except:
-            pass
-        __build_type__ = val
-    return __build_type__
+def load_release():
+    global __release__
+    import pkg_resources
+    val = "unknown"
+    try:
+        dist = pkg_resources.get_distribution("checkmate")
+        match = re.search('((\d+\.)+)(\D.+)', dist.version)
+        if match:
+            val = match.group(match.lastindex)
+    except:
+        pass
+    __release__ = val
+
+load_release()
 
 
 def version():
-    return "%s-%s-%s" % (__version__, __release__, get_build_type())
+    return "%s-%s" % (__version__, __release__)
 
 
 print(version())
