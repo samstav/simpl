72a0cde60ad63ec8c6554130a667d90efacbe505
ziad-sawalha <ziad.sawalha@rackspace.com>
2012-11-20 14:37:21 -0600

    Support Github Proxy Calls
    - Allows UI to query github and bypass CORS.
    - Call must include X-Target-Url pointing to
      github server

--- rook/middleware.py
+++ rook/middleware.py
@@ -19,7 +19,7 @@ LOG = logging.getLogger(__name__)
 
 from rook.db import get_driver
 from checkmate.utils import HANDLERS, RESOURCES, STATIC, write_body, \
-        read_body, support_only, with_tenant, to_json, to_yaml, \
+        read_body, support_only, \
         get_time_string, import_class
 
 __version_string__ = None
@@ -44,7 +44,8 @@ class BrowserMiddleware(object):
         self.app = app
         STATIC.extend(['favicon.ico', 'apple-touch-icon.png', 'js', 'libs',
                        'css', 'img', 'authproxy', 'marketing', '', None,
-                       'feedback', 'partials'])
+                       'feedback', 'partials', 'githubproxy'])
+        HANDLERS['application/vnd.github.v3.raw'] = write_raw
         self.proxy_endpoints = proxy_endpoints
         self.with_simulator = with_simulator
         connection_string = os.environ.get('CHECKMATE_CONNECTION_STRING',
@@ -180,6 +181,67 @@ class BrowserMiddleware(object):
 
             return write_body(content, request, response)
 
+        @get('/githubproxy/<path:path>')
+        @support_only(['application/json',
+                       'application/vnd.github.v3.raw'])
+        def githubproxy(path=None):
+            """Proxy Github Requests
+
+            The Ajax client cannot talk to remote github servers because of
+            CORS. This function proxies these calls through this server.
+
+            The target server URL should be passed in through the
+            X-Target-Url header.
+            """
+            source = request.get_header('X-Target-Url')
+            if not source:
+                abort(406, "X-Target-Url header not supplied. The header is "
+                        "required and must point to a valid and permitted "
+                        "git endpoint.")
+
+            url = urlparse(source)
+            if url.scheme == 'https':
+                http_class = httplib.HTTPSConnection
+                port = url.port or 443
+            else:
+                http_class = httplib.HTTPConnection
+                port = url.port or 80
+            host = url.hostname
+
+            http = http_class(host, port)
+            headers = {
+                'Accept': request.get_header('Accept', ['application/json']),
+                }
+            body = None
+            try:
+                LOG.debug('Proxying github call to %s' % source)
+                http.request('GET', '/%s' % path, headers=headers)
+                resp = http.getresponse()
+                body = resp.read()
+            except Exception, e:
+                LOG.error('HTTP connection exception: %s' % e)
+                raise HTTPError(401, output='Unable to communicate with '
+                        'github server')
+            finally:
+                http.close()
+
+            if resp.status != 200:
+                LOG.debug('Invalid github call: %s' % resp.reason)
+                raise HTTPError(resp.status, output=resp.reason)
+
+            if 'application/json' in resp.getheader('Content-type'):
+                try:
+                    content = json.loads(body)
+                except ValueError:
+                    msg = 'Github did not return json-encoded body'
+                    LOG.debug(msg)
+                    raise HTTPError(resp.status, output=msg)
+            else:
+                content = body
+
+            return write_body(content, request, response)
+
+
         @post('/feedback')
         @support_only(['application/json'])
         def feedback():
@@ -216,3 +278,9 @@ class BrowserMiddleware(object):
                             e.get('HTTP_X_REQUESTED_WITH') != 'XMLHttpRequest':
                         e['PATH_INFO'] = "/"  # return client app
         return self.app(e, h)
+
+
+def write_raw(data, request, response):
+    """Write output in raw format"""
+    response.set_header('content-type', 'application/vnd.github.v3.raw')
+    return data
