3eddbc1adf1b3e4dc476e072a4baaac164403709
ziad-sawalha <ziad.sawalha@rackspace.com>
2012-12-11 04:24:22 -0600

    Provider Package Tests
    - rename test to more accurately represent what it
      was testing (the package, not a single provider)
    - test the opscode package as well

---
+++ checkmate/providers/opscode/tests/test_package.py
@@ -0,0 +1,27 @@
+import unittest2 as unittest
+
+from checkmate.providers import opscode
+from checkmate.providers import base
+
+
+class TestPackage(unittest.TestCase):
+    def test_package_registration(self):
+        base.PROVIDER_CLASSES = {}
+        opscode.register()
+        self.assertIn('opscode.chef-server', base.PROVIDER_CLASSES)
+        self.assertIn('opscode.chef-local', base.PROVIDER_CLASSES)
+        self.assertIn('opscode.chef-solo', base.PROVIDER_CLASSES)
+        self.assertEqual(len(base.PROVIDER_CLASSES), 3, msg="Check that all "
+                         "your providers are registered and tested for")
+
+
+if __name__ == '__main__':
+    # Run tests. Handle our parameters separately
+    import sys
+    args = sys.argv[:]
+    # Our --debug means --verbose for unittest
+    if '--debug' in args:
+        args.pop(args.index('--debug'))
+        if '--verbose' not in args:
+            args.insert(1, '--verbose')
+    unittest.main(argv=args)
