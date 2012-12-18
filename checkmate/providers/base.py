c35796dcc8b112d0235a007b3e4dd9121bdb2031
Ziad Sawalha <ziad.sawalha@rackspace.com>
2012-12-18 05:20:57 -0600

    ProviderBase: docs and cleanup

--- checkmate/providers/base.py
+++ checkmate/providers/base.py
@@ -1,11 +1,10 @@
 import logging
+import platform
 import random
 import string
 import uuid
-import checkmate
-import os
-import platform
 
+import checkmate
 from checkmate import utils
 from checkmate.common import schema
 from checkmate.components import Component
@@ -290,10 +289,10 @@ class ProviderBase(ProviderBasePlanningMixIn, ProviderBaseWorkflowMixIn):
             results = self._dict['provides']
         else:
             data = self.get_catalog(context)
-            for key, value in data.iteritems():
+            for key, type_category in data.iteritems():
                 if key == 'lists':
                     continue
-                for id, component in value.iteritems():
+                for id, component in type_category.iteritems():
                     if 'provides' in component:
                         for entry in component['provides']:
                             if entry not in results:
@@ -385,7 +384,8 @@ class ProviderBase(ProviderBasePlanningMixIn, ProviderBaseWorkflowMixIn):
         """Evaluate an option value.
 
         Understands the following functions:
-        - generate('uuid')
+        - generate_password()
+        - generate_uuid()
         """
         if function_string.startswith('generate_uuid('):
             return uuid.uuid4().hex
