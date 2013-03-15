e473e097d1bce423ea65df74ff78e64c8fed343b
andr5956 <andrew.plunk@rackspace.com>
2013-03-15 14:31:30 -0500

    fix pull request branch path

--- tools/test_pull_request.py
+++ tools/test_pull_request.py
@@ -122,7 +122,7 @@ if len(TESTS_PASSED) + len(TESTS_FAILED) > 0:
     print "Failed branch commit detail:"
     for branch in TESTS_FAILED:
         print "Branch %s:" % branch
-        bash("git log master.." + branch)
+        bash("git log master..pr/" + branch)
         print
 
     with open(TESTED_PULL_REQUEST_PATH, 'a') as tested_pull_request_file:
