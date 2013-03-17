db951f98b42361e27b3315c7d3b261306d9fb830
Ziad Sawalha <ziad.sawalha@rackspace.com>
2013-03-16 22:48:54 -0500

    Blueprint Schema Compatibility
    Note: this adds a 'version' field to blueprints
    just to detect and track the conversion. This is
    not the proposed versioning scheme and will be
    changed.
    
    - temporarily add a 'version' attribute. I expect
      this will be changed before the next release
    - detect pre-v0.7 blueprints and convert them
    - detect future versions and reject them

--- checkmate/blueprints.py
+++ checkmate/blueprints.py
@@ -89,7 +89,61 @@ class Blueprint(ExtensibleDict):
     methods.
     """
     def __init__(self, *args, **kwargs):
-        ExtensibleDict.__init__(self, *args, **kwargs)
+        obj = dict(*args, **kwargs)
+        converters = {
+            'v0.6': self.from_v0_6,
+            }
+        version = obj.get('version', 'v0.6')
+        if version in converters:
+            converters[version](obj)
+        elif version != 'v0.7':
+            raise CheckmateValidationException("This server does not support "
+                                               "version '%s' blueprints" %
+                                               version)
+        ExtensibleDict.__init__(self, obj)
+
+    @classmethod
+    def from_v0_6(cls, data):
+        """
+
+        Convert a pre v0.7 blueprint to a v0.7 one
+
+        Handles the following option changes:
+        - no select or comobo types (convert them to strings)
+        - no regex attirbute (move to constraint)
+        - no protocols attribute (move to constraint)
+
+        """
+        for option in data.get('options', {}).values():
+            if 'regex' in option:
+                constraint = {'regex': option['regex']}
+                if 'constraints' not in option:
+                    option['constraints'] = [constraint]
+                else:
+                    option['constraints'].append(constraint)
+                del option['regex']
+                LOG.warn("Converted 'regex' attribute in an option in "
+                         "blueprint '%s'" % data.get('id'))
+            if option.get('type') in ['select', 'combo']:
+                option['type'] = 'string'
+                LOG.warn("Converted 'type' from 'select' or 'combo' to "
+                         "'string'  in blueprint '%s'" % data.get('id'))
+            if option.get('type') == 'int':
+                option['type'] = 'integer'
+                LOG.warn("Converted 'type' from 'int' to 'integer' "
+                         " in blueprint '%s'" % data.get('id'))
+            if 'protocols' in option:
+                constraint = {'protocols': option['protocols']}
+                if 'constraints' not in option:
+                    option['constraints'] = [constraint]
+                else:
+                    option['constraints'].append(constraint)
+                del option['protocols']
+                LOG.warn("Converted 'protocols' attribute in an option in "
+                         "blueprint '%s'" % data.get('id'))
+        if data.get('version', 'v0.1') < 'v0.7':
+            data['version'] = 'v0.7'
+        return data
 
     @classmethod
     def validate(cls, obj):
