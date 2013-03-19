5e51624cbf7bbca9b711ec47682ceebc2582f21c
andr5956 <andrew.plunk@rackspace.com>
2013-03-19 14:12:53 -0500

    split otu into function script

--- tools/test_pull_request_trigger.py
+++ tools/test_pull_request_trigger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from test_pull_request import bash, setup_pull_request_branches, teardown_pull_request_branches
+from test_functions import *
 
 if len(setup_pull_request_branches()) > 0:
     bash("wget -O - http://cimaster-n01.cloudplatform.rackspace.net:8080/job/checkmate-test-pull-request/build")
