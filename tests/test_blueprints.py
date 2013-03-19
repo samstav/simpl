0d0f0bd9423c68dbe6b087e523a01dbb137b22a1
andr5956 <andrew.plunk@rackspace.com>
2013-03-19 12:59:58 -0500

    force syntax error

--- tests/test_blueprints.py
+++ tests/test_blueprints.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import logging
 import unittest2 as unittest
-
+syntax_error
 # Init logging before we load the database, 3rd party, and 'noisy' modules
 from checkmate.utils import init_console_logging
 init_console_logging()
