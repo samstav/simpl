3db67e03e92eb9ff17a2a1dd2dbc9eedc87f2a63
andr5956 <andrew.plunk@rackspace.com>
2013-03-19 14:25:38 -0500

    fix tested pull request var scope

--- tools/test_pull_request_trigger.py
+++ tools/test_pull_request_trigger.py
@@ -4,6 +4,6 @@ import test_functions
 TESTED_PULL_REQUEST_PATH = "tools/tested_pull_requests"
 
 if len(test_functions.setup_pull_request_branches(TESTED_PULL_REQUEST_PATH)) > 0:
-    bash("wget -O - http://cimaster-n01.cloudplatform.rackspace.net:8080/job/checkmate-test-pull-request/build")
+    test_functions.bash("wget -O - http://cimaster-n01.cloudplatform.rackspace.net:8080/job/checkmate-test-pull-request/build")
 
 teardown_pull_request_branches()
