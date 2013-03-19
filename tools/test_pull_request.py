78308b2a539026655ffb34df8fb5df87675b2384
andr5956 <andrew.plunk@rackspace.com>
2013-03-19 12:47:07 -0500

    make job fail on failed tests

--- tools/test_pull_request.py
+++ tools/test_pull_request.py
@@ -73,26 +73,22 @@ print "TESTED_PULL_REQUESTS %s" % " ,".join(TESTED_PULL_REQUESTS)
 PULL_REQUESTS = [pr for pr in REMOTE_PULL_REQUESTS 
                     if pr not in TESTED_PULL_REQUESTS]
 
-if len(PULL_REQUESTS) > 0:
 
-    for branch in PULL_REQUESTS:
-        pr_branch = "pr/%s" % branch
-        bash("git checkout %s" % pr_branch)
 
-        try:
-            test()
-            TESTS_PASSED.append(branch)
-        except subprocess.CalledProcessError as cpe:
-            print cpe.output
-            print "Pull Request %s failed!" % branch
-            SUCCESS = False
-            TESTS_FAILED.append(branch)
+for branch in PULL_REQUESTS:
+    pr_branch = "pr/%s" % branch
+    bash("git checkout %s" % pr_branch)
 
-        bash("git checkout master")
-else:
-    #if no tests were run we need to make a no-op test output file or jenkins will fail
-    with open("nosetests.xml", 'w') as nosetests:
-        nosetests.write('<?xml version="1.0" encoding="UTF-8"?><testsuite name="nosetests" tests="0" errors="0" failures="0" skip="0"></testsuite>')
+    try:
+        test()
+        TESTS_PASSED.append(branch)
+    except subprocess.CalledProcessError as cpe:
+        print cpe.output
+        print "Pull Request %s failed!" % branch
+        SUCCESS = False
+        TESTS_FAILED.append(branch)
+
+    bash("git checkout master")
 
 bash("mv .git/config.bak .git/config")
 
