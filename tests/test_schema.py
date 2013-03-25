3e753f060c8f39298f0746ad79cb40d54f8e76f7
Ziad Sawalha <ziad.sawalha@rackspace.com>
2013-03-25 11:45:16 -0500

    Validate URL Inputs

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -2,6 +2,7 @@
 import unittest2 as unittest
 
 from checkmate.common import schema
+from checkmate import utils
 
 #Commented out test correspond to removal of '.-_' translations
 
@@ -61,7 +62,6 @@ class TestSchema(unittest.TestCase):
                 'choice': [],
                 'description': "Yada yada",
                 'required': False,
-                'type': "string",
                 'constrains': [],
                 'constraints': [],
                 'display-hints': {},
@@ -82,6 +82,79 @@ class TestSchema(unittest.TestCase):
 #    def test_translation_combined(self):
 #        self.assertEqual(schema.translate('dest.directory/pub_conf'),
 #                'destination_directory/public_configuration')
+
+    def test_validate_inputs(self):
+        """Test that known input formats all pass validation"""
+        deployment = utils.yaml_to_dict("""
+            blueprint:
+              options:
+                my_simple_url:
+                  type: url
+                my_complex_url:
+                  type: url
+                my_int:
+                  type: integer
+                my_boolean:
+                  type: boolean
+                my_string:
+                  type: string
+            inputs:
+              blueprint:
+                my_simple_url: http://domain.com
+                my_complex_url:
+                  url: https://secure.domain.com
+                  certificate: |
+                    ----- BEGIN ....
+                my_int: 1
+                my_boolean: true
+                my_string: Hello!
+            """)
+        self.assertListEqual(schema.validate_inputs(deployment), [])
+
+    def test_validate_inputs_url_negative(self):
+        """Test that bad url input formats don't pass validation"""
+        deployment = utils.yaml_to_dict("""
+            blueprint:
+              options:
+                try_list:
+                  type: url
+                bad_fields:
+                  type: url
+                try_int:
+                  type: url
+                try_boolean:
+                  type: url
+            inputs:
+              blueprint:
+                try_list:
+                - url: http://domain.com
+                - certificate: blah
+                bad_fields:
+                  foo: https://secure.domain.com
+                  bar: |
+                    ----- BEGIN ....
+                try_int: 1
+                try_boolean: true
+            """)
+        expected = [
+            ("'foo' not a valid value. Only url, protocol, scheme, netloc, "
+             "hostname, port, path, certificate, private_key, "
+             "intermediate_key allowed"),
+            ("'bar' not a valid value. Only url, protocol, scheme, netloc, "
+             "hostname, port, path, certificate, private_key, "
+             "intermediate_key allowed"),
+            ("Option 'try_list' should be a string or valid url mapping. It "
+             "is a 'list' which is not valid"),
+            ("Option 'try_int' should be a string or valid url mapping. It is "
+             "a 'int' which is not valid"),
+            ("Option 'try_boolean' should be a string or valid url mapping. "
+             "It is a 'bool' which is not valid"),
+        ]
+        results = schema.validate_inputs(deployment)
+        results.sort()
+        expected.sort()
+        self.assertListEqual(results, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
