d5d4729cf21e647af7204b048c6f7345dcd44a4e
andr5956 <andrew.plunk@rackspace.com>
2013-03-25 09:21:38 -0500

    add unit tests, move generate resource name logic into central location

--- checkmate/plan.py
+++ checkmate/plan.py
@@ -235,8 +235,10 @@ class Plan(ExtensibleDict):
                                                 provider_key=provider.key,
                                                 resource_type=resource['type'],
                                                 default=default_domain)
+                
                 name = utils.generate_resource_name(deployment, "shared%s.%s" % 
                     (key, domain))
+
                 # Call provider to give us a resource template
                 result = (provider.generate_template(deployment,
                           resource['type'], None, context, name=name))
