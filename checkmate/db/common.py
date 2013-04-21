10dc0756fbee654490b988df13b113eace663601
Ziad Sawalha <ziad.sawalha@rackspace.com>
2013-04-21 08:07:44 -0500

    db.common: PEP8 + pylint

--- checkmate/db/common.py
+++ checkmate/db/common.py
@@ -5,7 +5,7 @@ from checkmate import utils
 
 DEFAULT_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data')
 ACTUAL_DATA_PATH = os.path.join(os.environ.get('CHECKMATE_DATA_PATH',
-                                          DEFAULT_DATA_PATH))
+                                               DEFAULT_DATA_PATH))
 
 from checkmate.db.base import DbBase as dbBaseClass
 DbBase = dbBaseClass
@@ -17,7 +17,8 @@ DEFAULT_RETRIES = 5
 #amount of time before a lock can be force deleted
 DEFAULT_STALE_LOCK_TIMEOUT = 10
 
-def any_id_problems(id):
+
+def any_id_problems(api_id):
     """Validates the ID provided is safe and returns problems as a string.
 
     To use this, call it with an ID you want to validate. If the response is
@@ -27,41 +28,41 @@ def any_id_problems(id):
                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
                           "0123456789"
     allowed_chars = allowed_start_chars + "-_.+~@"
-    if id is None:
+    if api_id is None:
         return 'ID cannot be blank'
-    if not isinstance(id, basestring):
-        id = str(id)
-    if 1 > len(id) > 32:
+    if not isinstance(api_id, basestring):
+        api_id = str(api_id)
+    if 1 > len(api_id) > 32:
         return "ID must be 1 to 32 characters"
-    if id[0] not in allowed_start_chars:
-        return "Invalid start character '%s'. ID can start with any of '%s'" \
-                % (id[0], allowed_start_chars)
-    for c in id:
-        if c not in allowed_chars:
-            return "Invalid character '%s'. Allowed charaters are '%s'" % (c,
-                                                                allowed_chars)
+    if api_id[0] not in allowed_start_chars:
+        return ("Invalid start character '%s'. ID can start with any of '%s'" %
+                (api_id[0], allowed_start_chars))
+    for char in api_id:
+        if char not in allowed_chars:
+            return ("Invalid character '%s'. Allowed charaters are '%s'" %
+                    (char, allowed_chars))
     return None
 
 
 def get_driver(name=None, reset=False):
     global DB
-    if reset: #Forces a hard reset of global variable
+    if reset:  # Forces a hard reset of global variable
         DB = None
     if DB is None:
         if not name:
             connection_string = os.environ.get('CHECKMATE_CONNECTION_STRING',
-                    'sqlite://')
+                                               'sqlite://')
             if connection_string.startswith('mongodb://'):
                 name = 'checkmate.db.mongodb.Driver'
             else:
                 name = 'checkmate.db.sql.Driver'
-        LOG.debug("Initializing database engine: %s" % name)
+        LOG.debug("Initializing database engine: %s", name)
         driver = utils.import_class(name)
         DB = driver()
     return DB
 
 
-def any_tenant_id_problems(id):
+def any_tenant_id_problems(api_id):
     """Validates the tenant provided is safe and returns problems as a string.
 
     To use this, call it with a tenant ID you want to validate. If the response
@@ -72,26 +73,29 @@ def any_tenant_id_problems(id):
                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
                           "0123456789"
     allowed_chars = allowed_start_chars + "-_.+~@()[]*&^=%$#!<>"
-    if id is None:
+    if api_id is None:
         return 'Tenant ID cannot be blank'
-    if not isinstance(id, basestring):
-        id = str(id)
-    if 0 > len(id) > 255:
+    if not isinstance(api_id, basestring):
+        api_id = str(api_id)
+    if 0 > len(api_id) > 255:
         return "Tenant ID must be 1 to 255 characters"
-    if id[0] not in allowed_start_chars:
-        return "Invalid start character '%s'. Tenant ID can start with any "\
-                "of '%s'" % (id[0], allowed_start_chars)
-    for c in id:
-        if c not in allowed_chars:
-            return "Invalid character '%s' in Tenant ID. Allowed charaters "\
-                    "are '%s'" % (c, allowed_chars)
+    if api_id[0] not in allowed_start_chars:
+        return ("Invalid start character '%s'. Tenant ID can start with any "
+                "of '%s'" % (api_id[0], allowed_start_chars))
+    for char in api_id:
+        if char not in allowed_chars:
+            return ("Invalid character '%s' in Tenant ID. Allowed charaters "
+                    "are '%s'" % (char, allowed_chars))
     return None
+
 
 class DatabaseTimeoutException(Exception):
     pass
 
+
 class ObjectLockedError(Exception):
     pass
+
 
 class InvalidKeyError(Exception):
     pass
