c52f9e606785945deeaf07683c32af669d6ddaa7
Richard Lee <rblee88@gmail.com>
2013-05-15 09:51:28 -0500

    Add some more auth service tests

--- rook/static/test/services/auth_spec.js
+++ rook/static/test/services/auth_spec.js
@@ -1,15 +1,40 @@
 describe('auth Service', function(){
 
+  var request, response, endpoint, user;
   beforeEach(module('checkmate.services'));
   beforeEach(inject(function(auth, $resource, $rootScope){
     this.auth = auth;
+    request = { getResponseHeader: emptyFunction };
+    user = {};
+    response = { access: { user: user, token: emptyFunction } };
+    endpoint = {};
   }));
 
-  it('should create an identity object based on request, response, and endpoint', function(){
-    request = { getResponseHeader: emptyFunction };
-    response = { access: { user: emptyFunction, token: emptyFunction } };
-    endpoint = {};
-    expect(this.auth.create_identity(request, response, endpoint)).not.toBe(null);
-  });
+  describe('create_identity', function(){
+    it('should create an identity object based on request, response, and endpoint', function(){
+      expect(this.auth.create_identity(request, response, endpoint)).not.toBe(null);
+    });
 
+    it('should assign is_admin to true if the X-AuthZ-Admin header is True', function(){
+      request = { getResponseHeader: function(){ return 'True'; } };
+      expect(this.auth.create_identity(request, response, endpoint).is_admin).toBeTruthy();
+    });
+
+    it('should assign is_admin to false if the X-AuthZ-Admin header is not "True"', function(){
+      request = { getResponseHeader: function(){ return 'Truish'; } };
+      expect(this.auth.create_identity(request, response, endpoint).is_admin).toBeFalsy();
+    });
+
+    it('should set username to the access user name if it is present', function(){
+      user = { name: 'batman' };
+      response = { access: { user: user, token: emptyFunction } };
+      expect(this.auth.create_identity(request, response, endpoint).username).toEqual('batman');
+    });
+
+    it('should set username to the access user id if user name is not present', function(){
+      user = { id: 'robin' };
+      response = { access: { user: user, token: emptyFunction } };
+      expect(this.auth.create_identity(request, response, endpoint).username).toEqual('robin');
+    });
+  });
 });
