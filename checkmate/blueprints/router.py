88cbd7a9d51c78b874e622e5ba79379cf71f3576
Ziad Sawalha <ziad.sawalha@rackspace.com>
2013-06-12 16:20:54 -0500

    Blueprints: Remove Unused Code + Fix Copy Error

--- checkmate/blueprints/router.py
+++ checkmate/blueprints/router.py
@@ -1,20 +1,14 @@
 '''
 Blueprints Router
 '''
-import os
 import logging
 
 #pylint: disable=E0611
 from bottle import request
 
 from checkmate import utils
-from checkmate import db
 
 LOG = logging.getLogger(__name__)
-DB = db.get_driver()
-SIMULATOR_DB = db.get_driver(connection_string=os.environ.get(
-    'CHECKMATE_SIMULATOR_CONNECTION_STRING',
-    os.environ.get('CHECKMATE_CONNECTION_STRING', 'sqlite://')))
 
 
 class Router(object):
@@ -32,9 +26,9 @@ class Router(object):
         app.route('/blueprints', 'GET', self.get_blueprints)
 
     @utils.with_tenant
-    @utils.formatted_response('deployments', with_pagination=True)
+    @utils.formatted_response('blueprints', with_pagination=True)
     def get_blueprints(self, tenant_id=None, offset=None, limit=None):
-        ''' Get existing deployments '''
+        ''' Get existing blueprints '''
         details = request.query.get('details')
         return self.manager.get_blueprints(
             tenant_id=tenant_id,
