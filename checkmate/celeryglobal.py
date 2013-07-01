3e04583072806aafef0ec5e005651fcd74537b70
Ziad Sawalha <ziad.sawalha@rackspace.com>
2013-07-01 02:27:41 -0500

    Config: Init Config when Celery Worker Starts

--- checkmate/celeryglobal.py
+++ checkmate/celeryglobal.py
@@ -10,19 +10,30 @@ TODO:
 
 '''
 #pylint: disable=W0611
-
 import logging
+import os
 
 from celery import Task
 from celery.exceptions import RetryTaskError
+from celery.signals import worker_process_init
 
+from checkmate.common import config
 from checkmate.db.common import InvalidKeyError, ObjectLockedError
 
 LOG = logging.getLogger(__name__)
+CONFIG = config.current()
 
 
-class AlwaysRetryTask(Task):  # pylint: disable=R0904
-    '''Base of retrying tasks
+@worker_process_init.connect  # pylint: disable=W0613
+def init_checkmate_worker(**kwargs):
+    '''Initialize Configuration.'''
+    LOG.info("Initializing Checkmate worker")
+    CONFIG.update(config.parse_environment(env=os.environ))
+    LOG.debug("Initialized config: %s", CONFIG.__dict__)
+
+
+class AlwaysRetryTask(Task):  # pylint: disable=R0904,W0223
+    '''Base of retrying tasks.
 
     See: https://groups.google.com/forum/?fromgroups=#!topic/celery-users/
          DACXXud_8eI
@@ -38,8 +49,8 @@ class AlwaysRetryTask(Task):  # pylint: disable=R0904
             return self.retry(exc=exc)
 
 
-class SingleTask(Task):  # pylint: disable=R0904
-    '''Base of non concurrent tasks'''
+class SingleTask(Task):  # pylint: disable=R0904,W0223
+    '''Base of non concurrent tasks.'''
     abstract = True
 
     def __call__(self, *args, **kwargs):
