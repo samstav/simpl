3219f4b5c7906f5f0caee852463fce10255e6a53
Paul Nelson <pablosan@salientblue.com>
2013-07-10 15:49:17 -0500

    re #822: Cleanup _content_to_deployment

--- tests/unit/test_deployments_router.py
+++ tests/unit/test_deployments_router.py
@@ -50,7 +50,7 @@ class TestAPICalls(unittest.TestCase):
         self.mox.StubOutWithMock(utils, 'read_body')
         utils.read_body(req).AndReturn({})
         self.mox.ReplayAll()
-        result = deployments.router._content_to_deployment(req,
+        result = deployments.router._content_to_deployment(request=req,
                                                            deployment_id="1",
                                                            tenant_id="A")
         self.mox.VerifyAll()
@@ -71,7 +71,7 @@ class TestAPICalls(unittest.TestCase):
         self.mox.StubOutWithMock(utils, 'read_body')
         utils.read_body(req).AndReturn({'created-by': 'tom'})
         self.mox.ReplayAll()
-        result = deployments.router._content_to_deployment(req,
+        result = deployments.router._content_to_deployment(request=req,
                                                            deployment_id="1",
                                                            tenant_id="A")
         self.mox.VerifyAll()
