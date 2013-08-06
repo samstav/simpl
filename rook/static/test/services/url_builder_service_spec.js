2ab48a698a8e5feeafd0114a80d9576683ad233b
Anderson Mesquita <andersonvom@thoughtworks.com>
2013-08-06 16:45:16 -0500

    Refactor urlBuilder tests: `this.urlBuilder` --> `urlBuilder`

--- rook/static/test/services/url_builder_service_spec.js
+++ rook/static/test/services/url_builder_service_spec.js
@@ -1,17 +1,12 @@
 describe('urlBuilder', function(){
-  var resource_id,
-      resource_type,
-      region,
-      tenantId,
+  var tenantId,
+      username,
       resource,
-      username;
+      urlBuilder;
 
   beforeEach(module('checkmate.services'));
-  beforeEach(inject(function(urlBuilder){
-    this.urlBuilder = urlBuilder;
-    resource_id = 'resource_id';
-    resource_type = 'server';
-    region = 'ORD';
+  beforeEach(inject(['urlBuilder', function(_urlBuilder){
+    urlBuilder = _urlBuilder;
     tenantId = '123';
     username = 'username';
     resource = {
@@ -19,35 +14,35 @@ describe('urlBuilder', function(){
         id: 'resource_id',
         public_ip: '0.0.0.0',
       },
-      provider: 'nova', // 'nova', 'legacy', 'load-balancer', 'databases', other
+      provider: 'nova', // 'legacy', 'load-balancer', 'databases', other
       region: 'ORD',
     };
-  }));
+  }]));
 
   describe('#get_url', function() {
     it('should get URLs for Cloud Control', function() {
-      var url = this.urlBuilder.get_url('cloud_control', resource, tenantId, username);
+      var url = urlBuilder.get_url('cloud_control', resource, tenantId, username);
       expect(url).toContain('cloudcontrol.rackspacecloud.com');
     });
 
     it('should get URLs for My Cloud', function() {
-      var url = this.urlBuilder.get_url('my_cloud', resource, tenantId, username);
+      var url = urlBuilder.get_url('my_cloud', resource, tenantId, username);
       expect(url).toContain('mycloud.rackspace.com');
     });
 
     it('should get URLs for Nova Stats', function() {
-      var url = this.urlBuilder.get_url('nova_stats', resource, tenantId, username);
+      var url = urlBuilder.get_url('nova_stats', resource, tenantId, username);
       expect(url).toContain('reports.ohthree.com');
     });
 
     it('should get URLs for SSH', function() {
-      var url = this.urlBuilder.get_url('ssh', resource, tenantId, username);
+      var url = urlBuilder.get_url('ssh', resource, tenantId, username);
       expect(url).toContain('ssh://');
     });
 
     it('should not build a URL is resource is not valid', function() {
       resource.provider = 'custom';
-      var url = this.urlBuilder.get_url('whatever', resource, tenantId, username);
+      var url = urlBuilder.get_url('whatever', resource, tenantId, username);
       expect(url).toBe(undefined);
     });
   });
@@ -55,14 +50,14 @@ describe('urlBuilder', function(){
   describe('novaStatsURL', function(){
     it('should build a nova stats url', function(){
       var expected = 'https://reports.ohthree.com/ord/instance/resource_id';
-      expect(this.urlBuilder.get_url('nova_stats', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('nova_stats', resource, tenantId, username)).toEqual(expected);
     });
   });
 
   describe('sshTo', function(){
     it('should build the ssh path to the given ip', function(){
       var expected = 'ssh://root@0.0.0.0';
-      expect(this.urlBuilder.get_url('ssh', resource, tenantId, username)).toEqual('ssh://root@0.0.0.0');
+      expect(urlBuilder.get_url('ssh', resource, tenantId, username)).toEqual('ssh://root@0.0.0.0');
     });
   });
 
@@ -74,25 +69,25 @@ describe('urlBuilder', function(){
     it('should build an open stack server url', function(){
       resource.provider = 'nova';
       var expected = 'https://mycloud.rackspace.com/a/username/#compute%2CcloudServersOpenStack%2CREGION_HERE/resource_id';
-      expect(this.urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should build a legacy server url', function(){
       resource.provider = 'legacy';
       var expected = 'https://mycloud.rackspace.com/a/username/#compute%2CcloudServers%2CREGION_HERE/resource_id';
-      expect(this.urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should build a database url', function(){
       resource.provider = 'databases';
       var expected = 'https://mycloud.rackspace.com/a/username/database#rax%3Adatabase%2CcloudDatabases%2CREGION_HERE/resource_id';
-      expect(this.urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should build a load balancer url', function(){
       resource.provider = 'load-balancer';
       var expected = 'https://mycloud.rackspace.com/a/username/load_balancers#rax%3Aload-balancer%2CcloudLoadBalancers%2CREGION_HERE/resource_id';
-      expect(this.urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('my_cloud', resource, tenantId, username)).toEqual(expected);
     });
   });
 
@@ -100,57 +95,57 @@ describe('urlBuilder', function(){
     it('should use the london url if region is london', function(){
       var expected = 'https://lon.cloudcontrol.rackspacecloud.com/customer/123/next_gen_servers/LON/resource_id';
       resource.region = 'LON';
-      expect(this.urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should default any given region other than london to US endpoint', function(){
       var expected = 'https://us.cloudcontrol.rackspacecloud.com/customer/123/next_gen_servers/ORD/resource_id';
-      expect(this.urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should build the url for a load balancer', function(){
       var expected = 'https://us.cloudcontrol.rackspacecloud.com/customer/123/load_balancers/ORD/resource_id';
       resource.provider = 'load-balancer';
-      expect(this.urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should build the url for a database', function(){
       var expected = 'https://us.cloudcontrol.rackspacecloud.com/customer/123/dbaas/instances/ORD/resource_id';
       resource.provider = 'databases';
-      expect(this.urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
     });
 
     it('should build the url for a legacy server', function(){
       var expected = 'https://us.cloudcontrol.rackspacecloud.com/customer/123/first_gen_servers/ORD/resource_id';
       resource.provider = 'legacy';
-      expect(this.urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
+      expect(urlBuilder.get_url('cloud_control', resource, tenantId, username)).toEqual(expected);
     });
   });
 
   describe('#is_valid', function() {
     it('should be valid if resource provider is nova', function() {
       resource.provider = 'nova';
-      expect(this.urlBuilder.is_valid(resource)).toBe(true);
+      expect(urlBuilder.is_valid(resource)).toBe(true);
     });
 
     it('should be valid if resource provider is legacy', function() {
       resource.provider = 'legacy';
-      expect(this.urlBuilder.is_valid(resource)).toBe(true);
+      expect(urlBuilder.is_valid(resource)).toBe(true);
     });
 
     it('should be valid if resource provider is load-balancer', function() {
       resource.provider = 'load-balancer';
-      expect(this.urlBuilder.is_valid(resource)).toBe(true);
+      expect(urlBuilder.is_valid(resource)).toBe(true);
     });
 
     it('should be valid if resource provider is databases', function() {
       resource.provider = 'databases';
-      expect(this.urlBuilder.is_valid(resource)).toBe(true);
+      expect(urlBuilder.is_valid(resource)).toBe(true);
     });
 
     it('should not be valid otherwise', function() {
       resource.provider = 'custom';
-      expect(this.urlBuilder.is_valid(resource)).toBe(false);
+      expect(urlBuilder.is_valid(resource)).toBe(false);
     });
   });
 });
