492cdd192ca6d91b6cdc8fe26987cf05653f3247
Brint O'Hearn <brint.ohearn@rackspace.com>
2013-08-09 11:22:48 -0500

    Fix for issue #1180

--- checkmate/common/crypto.py
+++ checkmate/common/crypto.py
@@ -1,10 +1,11 @@
+import crypt
 from Crypto import Random
-from Crypto.Hash import SHA512
 
 
 def HashSHA512(value, salt=None):
     if not salt:
-        salt = Random.get_random_bytes(8).encode('base64').strip()
-    h = SHA512.new(salt)
-    h.update(value)
-    return "$6$%s$%s" % (salt, h.hexdigest())
+        Random.atfork()
+        salt = "$6$" + Random.get_random_bytes(6).encode('base64')[:-1].replace('+', '.') + "$"
+    else:
+        salt = "$6$" + salt + "$"
+    return crypt.crypt(value, salt)
