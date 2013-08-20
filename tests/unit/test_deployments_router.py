83d7f49d0d01a8df607dfead5d64c7445ac3d74c
Richard Lee <rblee88@gmail.com>
2013-08-20 18:09:58 -0500

    Convert deployments router test to use mock

--- tests/unit/test_deployments_router.py
+++ tests/unit/test_deployments_router.py
@@ -5,8 +5,6 @@ import unittest
 
 import bottle
 import mock
-import mox
-from mox import IgnoreArg
 from webtest import TestApp
 
 os.environ['CHECKMATE_CONNECTION_STRING'] = 'sqlite://'
@@ -16,18 +14,13 @@ from checkmate import deployments, test, utils
 class TestAPICalls(unittest.TestCase):
 
     def setUp(self):
-        self.mox = mox.Mox()
-
         self.root_app = bottle.Bottle()
         self.root_app.catchall = False
         self.filters = test.MockWsgiFilters(self.root_app)
         self.app = TestApp(self.filters)
 
-        self.manager = self.mox.CreateMockAnything()
+        self.manager = mock.Mock()
         self.router = deployments.Router(self.root_app, self.manager)
-
-    def tearDown(self):
-        self.mox.UnsetStubs()
 
     def _assert_good_count(self, ret, expected_count):
         self.assertIsNotNone(ret, "No count returned")
@@ -35,18 +28,16 @@ class TestAPICalls(unittest.TestCase):
         self.assertEqual(expected_count, ret.get("count", -1),
                          "Wrong count returned")
 
-    def test_created_by_assigned(self):
-        req = self.mox.CreateMockAnything()
-        req.context = self.mox.CreateMockAnything()
+    @mock.patch.object(utils, 'read_body')
+    def test_created_by_assigned(self, mock_read):
+        req = mock.Mock()
+        req.context = mock.Mock()
         req.context.username = 'john'
         req.headers = {}
-        self.mox.StubOutWithMock(utils, 'read_body')
-        utils.read_body(req).AndReturn({})
-        self.mox.ReplayAll()
+        mock_read.return_value = {}
         result = deployments.router._content_to_deployment(request=req,
                                                            deployment_id="1",
                                                            tenant_id="A")
-        self.mox.VerifyAll()
         self.assertIn('created', result)
         expected = {
             'status': 'NEW',
@@ -56,30 +47,30 @@ class TestAPICalls(unittest.TestCase):
             'created': result['created'],
         }
         self.assertDictEqual(result, expected)
+        mock_read.assert_called_once_with(req)
 
-    def test_created_not_overwritten(self):
-        req = self.mox.CreateMockAnything()
-        req.context = self.mox.CreateMockAnything()
+    @mock.patch.object(utils, 'read_body')
+    def test_created_not_overwritten(self, mock_read):
+        req = mock.Mock()
+        req.context = mock.Mock()
         req.context.username = 'john'
         req.headers = {}
-        self.mox.StubOutWithMock(utils, 'read_body')
-        utils.read_body(req).AndReturn({'created-by': 'tom'})
-        self.mox.ReplayAll()
+        mock_read.return_value = {'created-by': 'tom'}
         result = deployments.router._content_to_deployment(request=req,
                                                            deployment_id="1",
                                                            tenant_id="A")
-        self.mox.VerifyAll()
         self.assertEqual(result['created-by'], 'tom')
+        mock_read.assert_called_once_with(req)
 
     def test_get_count(self):
-        self.manager.count(tenant_id="123").AndReturn(3)
-        self.mox.ReplayAll()
+        self.manager.count.return_value = 3
         res = self.app.get('/123/deployments/count')
         self.assertEqual(res.status, '200 OK')
         self.assertEqual(res.content_type, 'application/json')
         self._assert_good_count(json.loads(res.body), 3)
 
-    def test_post_asynchronous(self):
+    @mock.patch.object(deployments.router, 'tasks')
+    def test_post_asynchronous(self, mock_tasks):
         """ Test that POST /deployments?asynchronous=1 returns a 202 """
         deployment = {
             'id': '1234',
@@ -90,22 +81,24 @@ class TestAPICalls(unittest.TestCase):
                 'services': {}
             }
         }
-        self.manager.save_deployment(IgnoreArg(), api_id='1234',
-                                     tenant_id="T1000").AndReturn(None)
-        self.mox.StubOutWithMock(deployments.router, "tasks")
-        tasks = deployments.router.tasks
-        tasks.process_post_deployment = self.mox.CreateMockAnything()
-        tasks.process_post_deployment.delay(IgnoreArg(),
-                                            IgnoreArg()).AndReturn(None)
+        self.manager.save_deployment.return_value = None
 
-        self.mox.ReplayAll()
+        mock_tasks.process_post_deployment = mock.Mock()
+        mock_tasks.process_post_deployment.delay.return_value = None
+
         res = self.app.post('/T1000/deployments?asynchronous=1',
                             json.dumps(deployment),
                             content_type='application/json')
         self.assertEqual(res.status, '202 Accepted')
         self.assertEqual(res.content_type, 'application/json')
+        self.manager.save_deployment.assert_called_once_with(
+            mock.ANY,
+            api_id='1234',
+            tenant_id='T1000'
+        )
 
-    def test_post_synchronous(self):
+    @mock.patch.object(deployments.router, 'tasks')
+    def test_post_synchronous(self, mock_tasks):
         """ Test that POST /deployments returns a 202 """
         deployment = {
             'id': '1234',
@@ -116,64 +109,66 @@ class TestAPICalls(unittest.TestCase):
                 'services': {}
             }
         }
-        self.manager.select_driver('1234').AndReturn(self.manager)
-        self.manager.save_deployment(deployment, IgnoreArg(),
-                                     tenant_id="T1000").AndReturn(None)
-        self.mox.StubOutWithMock(deployments.router, "tasks")
-        tasks = deployments.router.tasks
-        tasks.process_post_deployment(IgnoreArg(), IgnoreArg(),
-                                      driver=IgnoreArg()).AndReturn(None)
+        self.manager.select_driver.return_value = self.manager
+        mock_tasks.process_post_deployment.return_value = None
 
-        self.mox.ReplayAll()
         res = self.app.post('/T1000/deployments',
                             json.dumps(deployment),
                             content_type='application/json')
         self.assertEqual(res.status, '202 Accepted')
         self.assertEqual(res.content_type, 'application/json')
+        self.manager.select_driver.assert_called_once_with('1234')
 
-    def test_get_deployment_secrets_ok(self):
+    @mock.patch.object(utils, 'write_body')
+    @mock.patch('bottle.request')
+    def test_get_deployment_secrets_ok(self, mock_request, mock_write_body):
         deployment = {
             'id': '1234',
             'tenantId': 'T1000',
         }
-        self.manager.get_deployment('1234', tenant_id="T1000")\
-            .AndReturn(deployment)
+        self.manager.get_deployment.return_value = deployment
 
-        context = self.mox.CreateMockAnything()
+        context = mock.Mock()
         context.is_admin = True
-        self.mox.StubOutWithMock(bottle.request, "context")
-        bottle.request.context = context
+        mock_request.context = context
         data = {'foo': 1}
-        self.manager.get_deployment_secrets('1234', tenant_id="T1000")\
-            .AndReturn(data)
-        self.mox.StubOutWithMock(deployments.router.utils, "write_body")
-        utils.write_body(data, bottle.request, bottle.response).AndReturn(42)
+        self.manager.get_deployment_secrets.return_value = data
+        mock_write_body.return_value = 42
 
-        self.mox.ReplayAll()
         result = self.router.get_deployment_secrets('1234', tenant_id="T1000")
+        self.manager.get_deployment.assert_called_once_with('1234', tenant_id='T1000')
+        self.manager.get_deployment_secrets.assert_called_once_with('1234', tenant_id='T1000')
         self.assertEqual(42, result)
 
-    def test_get_deployment_secrets_not_admin(self):
+    @mock.patch.object(utils, 'write_body')
+    @mock.patch('bottle.request')
+    def test_get_deployment_secrets_not_admin(self, mock_request, mock_write):
         deployment = {
             'id': '1234',
             'tenantId': 'T1000',
         }
-        self.manager.get_deployment('1234', tenant_id="T1000")\
-            .AndReturn(deployment)
+        self.manager.get_deployment.return_value = deployment
 
-        context = self.mox.CreateMockAnything()
+        context = mock.Mock()
         context.is_admin = False
-        self.mox.StubOutWithMock(bottle.request, "context")
-        bottle.request.context = context
+        mock_request.context = context
         data = {'foo': 1}
-        self.manager.get_deployment_secrets('1234', tenant_id="T1000")\
-            .AndReturn(data)
-        self.mox.StubOutWithMock(deployments.router.utils, "write_body")
-        utils.write_body(data, bottle.request, bottle.response).AndReturn(42)
+        self.manager.get_deployment_secrets.return_value = data
+        mock_write.return_value = 42
 
-        self.mox.ReplayAll()
         with self.assertRaises(bottle.HTTPError):
             self.router.get_deployment_secrets('1234', tenant_id="T1000")
+
+            self.manager.get_deployment.assert_called_once_with(
+                '1234', tenant_id='T1000'
+            )
+            mock_write.assert_called_once_with(data, bottle.request,
+                                               bottle.response)
+            self.manager.get_deployment_secrets.assert_called_once_with(
+                    '1234',
+                    tenant_id='T1000'
+            )
+
 
 
 class TestDeploymentRouter(unittest.TestCase):
