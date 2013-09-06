a5374bd8dbb1fc749e094f03d63f0687ab0898aa
Caleb Groom <cgroom@rackspace.com>
2013-09-06 14:25:33 -0500

    pylint

--- checkmate/providers/opscode/knife.py
+++ checkmate/providers/opscode/knife.py
@@ -832,8 +832,8 @@ def delete_environment(name, path=None):
             LOG.warn("Environment directory %s does not exist", full_path,
                      exc_info=True)
         else:
-            msg = "Could not delete environment %s. Reason '%s'. Error "
-            "Number %s" % (full_path, ose.strerror, ose.errno)
+            msg = ("Could not delete environment %s. Reason '%s'. Error "
+                   "Number %s" % (full_path, ose.strerror, ose.errno))
             raise cmexc.CheckmateUserException(msg, utils.get_class_name(
                 cmexc.CheckmateException), cmexc.UNEXPECTED_ERROR, '')
 
@@ -858,8 +858,9 @@ def delete_cookbooks(name, service_name, path=None):
                     LOG.warn("Cookbooks directory %s does not exist", path,
                              exc_info=True)
                 else:
-                    msg = "Could not delete cookbooks directory %s. Reason "
-                    "'%s'. Error Number %s" % (path, ose.strerror, ose.errno)
+                    msg = ("Could not delete cookbooks directory %s. Reason "
+                           "'%s'. Error Number %s" % (path, ose.strerror,
+                                                      ose.errno))
                     raise cmexc.CheckmateUserException(
                         msg,
                         utils.get_class_name(cmexc.CheckmateException),
