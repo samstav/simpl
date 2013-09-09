7853636a8ccd0d636f03f65bf3d76737d890600c
Ziad Sawalha <gerrit@highbridgellc.com>
2013-09-09 04:48:34 -0500

    script: add tasks

---
+++ tests/unit/providers/core/test_script_tasks.py
@@ -0,0 +1,55 @@
+# pylint: disable=C0103,R0201,R0904,W0212,W0613
+
+# Copyright (c) 2011-2013 Rackspace Hosting
+# All Rights Reserved.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Unit Tests for the core script provider tasks."""
+
+import logging
+
+import mock
+import unittest
+
+
+from checkmate import middleware
+from checkmate.providers.core.script import tasks
+
+LOG = logging.getLogger(__name__)
+
+
+class TestScriptTasks(unittest.TestCase):
+    """Class to test core.script celery tasks."""
+    def test_create_resource_simulation(self):
+        api = mock.Mock()
+        tasks.create_resource.provider = mock.Mock(return_value=api)
+        tasks.create_resource.callback = mock.Mock(return_value=None)
+        context = {
+            'simulation': True,
+            'region': 'NOOP',
+            'resource_key': '0',
+        }
+        context = middleware.RequestContext(**context)
+        expected_result = {'instance:0': {'instance': {'A': 1}}}
+        results = tasks.create_resource(
+            context, 'D1', {'desired': {'A': 1}}, 'localhost', 'root')
+        self.assertEqual(expected_result, results)
+        tasks.create_resource.callback.assert_called_with(
+            context, {'instance': {'A': 1}})
+
+
+if __name__ == '__main__':
+    # Any change here should be made in all test files
+    from checkmate import test
+    import sys
+    test.run_with_params(sys.argv[:])
