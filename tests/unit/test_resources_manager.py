aff81a751e27db5ac604fce6c8c93f3de40821b4
Richard Lee <rblee88@gmail.com>
2013-09-11 15:41:16 -0500

    Add tests for resources manager

--- tests/unit/test_resources_manager.py
+++ tests/unit/test_resources_manager.py
@@ -4,17 +4,22 @@ import unittest
 from checkmate import resources
 
 
-class TestResourcesManager(unittest.TestCase):
-    def test_get_resources(self):
+class TestResourcesManagerGetResources(unittest.TestCase):
+    def setUp(self):
         driver = mock.Mock()
         manager = resources.Manager({'default': driver})
         manager.get_resources(tenant_id=123, offset=1, limit=3,
-                              resource_type='load-balancer',
-                              provider='load-ballooncer')
-        driver.get_resources.assert_called_once_with(
-            tenant_id=123,
-            offset=1,
-            limit=3,
-            resource_type='load-balancer',
-            provider='load-ballooncer'
-        )
+                              query='fake query')
+        _, self.kwargs = driver.get_resources.call_args
+
+    def test_pass_tenant_id_to_driver(self):
+        self.assertEqual(self.kwargs['tenant_id'], 123)
+
+    def test_pass_offset_to_driver(self):
+        self.assertEqual(self.kwargs['offset'], 1)
+
+    def test_pass_limit_to_driver(self):
+        self.assertEqual(self.kwargs['limit'], 3)
+
+    def test_pass_query_to_driver(self):
+        self.assertEqual(self.kwargs['query'], 'fake query')
