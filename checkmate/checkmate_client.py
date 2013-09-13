732c38eba264231fdad4ad8f5a3f16a517d3d4ed
Paul Nelson <pablosan@salientblue.com>
2013-09-13 00:01:43 -0500

    Add CONFIG.initialize() to client and queue

--- checkmate/checkmate_client.py
+++ checkmate/checkmate_client.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
 
+from checkmate.common import config
+CONFIG = config.current()
+
 print """
 *** Checkmate Command-Line Client Utility ***
 
@@ -17,6 +20,7 @@ Settings:
 
 
 def main_func():
+    CONFIG.initialize()
     for key in os.environ:
         if key.startswith('CHECKMATE_CLIENT'):
             print key, '=', os.environ[key]
