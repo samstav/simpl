436c856cf773e89306f4929d5b9a59ffb424e1ab
Ziad Sawalha <gerrit@highbridgellc.com>
2013-09-16 14:15:28 -0500

    Minor Updates

--- tests/unit/test_resources_manager.py
+++ tests/unit/test_resources_manager.py
@@ -1,3 +1,21 @@
+# pylint: disable=C0103,R0904,W0201,C0111
+
+# Copyright (c) 2011-2013 Rackspace Hosting
+# All Rights Reserved.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Tests for Resources Manager."""
+
 import mock
 import unittest
 
@@ -29,3 +47,9 @@ class TestResourcesManagerGetResources(unittest.TestCase):
 
     def test_pass_query_to_driver(self):
         self.assertEqual(self.kwargs['query'], 'fake query')
+
+
+if __name__ == '__main__':
+    import sys
+    from checkmate import test
+    test.run_with_params(sys.argv[:])
--- tests/unit/test_resources_router.py
+++ tests/unit/test_resources_router.py
@@ -1,3 +1,21 @@
+# pylint: disable=C0103,R0904,W0201,C0111
+
+# Copyright (c) 2011-2013 Rackspace Hosting
+# All Rights Reserved.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Tests for Resources Router."""
+
 import mock
 import unittest
 
@@ -51,3 +69,9 @@ class TestResourcesRouterGetResources(unittest.TestCase):
         self.app.get('/123/resources?id=1&id=2&id=foobar')
         _, kwargs = self.manager.get_resources.call_args
         self.assertEqual(kwargs['query']['resource_ids'], ['1', '2', 'foobar'])
+
+
+if __name__ == '__main__':
+    import sys
+    from checkmate import test
+    test.run_with_params(sys.argv[:])
