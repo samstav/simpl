8a74fe8caaa995f18a70205b0c8d005479c13698
Ziad Sawalha <gerrit@highbridgellc.com>
2013-09-26 22:12:44 -0500

    docs: Sphinx config

--- build_docs.sh
+++ build_docs.sh
@@ -3,9 +3,15 @@
 # Build Checkmate HTML Documentation
 #
 
+# Build the checkmate package
 python setup.py build
-sphinx-apidoc -F -o docs build/lib/checkmate
+
+# Generate the automatic docs from the modules
+sphinx-apidoc -f -o docs build/lib/checkmate
+
+# Build the full set of docs (API docs and other docs we create manually)
 cd docs
 make html
 cd ..
 
+echo "To view the docs in a browser: open docs/.build/html/index.html"
