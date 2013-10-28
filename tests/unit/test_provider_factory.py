3f681d1321014300b254655de4dbb9f10ba93e2d
Vineeth Venudasan <vineethv@thoughtworks.com>
2013-10-28 17:02:38 +0530

    Pylint ignore for test

--- tests/unit/test_provider_factory.py
+++ tests/unit/test_provider_factory.py
@@ -11,7 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-# pylint: disable=R0903
+# pylint: disable=R0903,C0111,W0613,R0904
 
 import mock
 import unittest
