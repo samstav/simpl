4dc05000a8ac1457855fd130dbe183f9ae4c21d9
Ziad Sawalha <gerrit@highbridgellc.com>
2015-01-06 00:25:22 -0600

    fix(lb): add desired-state schema item

--- checkmate/providers/rackspace/loadbalancer/provider.py
+++ checkmate/providers/rackspace/loadbalancer/provider.py
@@ -630,7 +630,6 @@ class Provider(rsbase.RackspaceProviderBase):
         """Return stored/override catalog if it exists, else connect, build,
         and return one.
         """
-
         # TODO(any): maybe implement this an on_get_catalog so we don't have to
         #        do this for every provider
         results = base.ProviderBase.get_catalog(self, context,
