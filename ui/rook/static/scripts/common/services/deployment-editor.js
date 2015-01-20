510b42ab7019643134884e99923cd19989720fe0
Chris Burrell <chrisburrell@gmail.com>
2015-01-20 16:07:26 -0600

    feat(deployment-editor): adds yaml/json button

--- ui/rook/static/scripts/common/services/deployment-editor.js
+++ ui/rook/static/scripts/common/services/deployment-editor.js
@@ -4,12 +4,36 @@ angular.module('checkmate.DeploymentData')
       restrict: 'E',
       replace: true,
       scope: {},
-      template: '<checkmate-codemirror checkmate-codemirror-opts="codemirror.options" ng-model="deployment"></checkmate-codemirror>',
+      template: '<div class="deployment-editor">\
+                  <checkmate-codemirror checkmate-codemirror-opts="codemirror.options" ng-model="deployment" ui-refresh="codemirror.options.mode"></checkmate-codemirror>\
+                  <div class="toggle-editor btn-group">\
+                    <button class="btn btn-mini" \
+                            ng-click="codemirror.toggleMode()"\
+                            ng-disabled="codemirror.options.mode == \'text/x-yaml\'"\
+                            disabled="disabled">\
+                      YAML\
+                    </button>\
+                    <button class="btn btn-mini"\
+                            ng-click="codemirror.toggleMode()"\
+                            ng-disabled="codemirror.options.mode == \'application/json\'">\
+                    JSON\
+                    </button>\
+                  </div>\
+                </div>',
       controller: function($scope) {
         $scope.deployment = '';
-        $scope.valid = true; // Is the YAML valid?
-        $scope.dirty = false; // Out of sync with topology
-        $scope.submitting = false; // Waiting on response?
+
+        var _to_yaml = function() {
+          $scope.deployment = jsyaml.safeDump(JSON.parse($scope.deployment));
+          $scope.codemirror.options.mode = 'text/x-yaml';
+          $scope.codemirror.foldFunction = CodeMirror.newFoldFunction(CodeMirror.fold.indent);
+        };
+
+        var _to_json = function() {
+          $scope.deployment = JSON.stringify(jsyaml.safeLoad($scope.deployment), undefined, 2);
+          $scope.codemirror.options.mode = 'application/json';
+          $scope.codemirror.foldFunction = CodeMirror.newFoldFunction(CodeMirror.fold.brace);
+        };
 
         $scope.codemirror = {
           editor: null,
@@ -18,23 +42,34 @@ angular.module('checkmate.DeploymentData')
           markAltered: function() {
             $scope.codemirror.editorAltered = true;
           },
+          toggleMode: function() {
+            try {
+              if ($scope.codemirror.options.mode == 'application/json') {
+                _to_yaml();
+              } else {
+                _to_json();
+              }
+            } catch(e) {
+              console.error(e);
+            }
+          },
           foldFunction: CodeMirror.newFoldFunction(CodeMirror.fold.indent),
           foldDefault: function(editor) {
-            var inBlueprint = false;
-            editor.eachLine(function(line) {
-              if (line.text.substring(0, 1) !== ' ') {
-                if (line.text.substring(0, 10) == 'blueprint:') {
-                  inBlueprint = true;
-               } else {
-                  inBlueprint = false;
-                  $scope.codemirror.foldFunction(editor, editor.getLineNumber(line));
-                }
-              } else if (inBlueprint && line.text.substring(0, 10) === '  options:') {
-                $scope.codemirror.foldFunction(editor, editor.getLineNumber(line));
-              } else if (inBlueprint && line.text.substring(0, 12) === '  meta-data:') {
-                $scope.codemirror.foldFunction(editor, editor.getLineNumber(line));
-              }
-            });
+            // var inBlueprint = false;
+            // editor.eachLine(function(line) {
+            //   if (line.text.substring(0, 1) !== ' ') {
+            //     if (line.text.substring(0, 10) == 'blueprint:') {
+            //       inBlueprint = true;
+            //    } else {
+            //       inBlueprint = false;
+            //       $scope.codemirror.foldFunction(editor, editor.getLineNumber(line));
+            //     }
+            //   } else if (inBlueprint && line.text.substring(0, 10) === '  options:') {
+            //     $scope.codemirror.foldFunction(editor, editor.getLineNumber(line));
+            //   } else if (inBlueprint && line.text.substring(0, 12) === '  meta-data:') {
+            //     $scope.codemirror.foldFunction(editor, editor.getLineNumber(line));
+            //   }
+            // });
           },
           setEditorDefaultState: function(editor) {
             $scope.codemirror.foldDefault(editor);
@@ -42,8 +77,8 @@ angular.module('checkmate.DeploymentData')
         };
 
         $scope.codemirror.options = {
-          lint: typeof CodeMirror.lint.yaml !== 'undefined',
-          mode: 'yaml',
+          lint: true,
+          mode: 'text/x-yaml',
           theme: 'lesser-dark',
           lineNumbers: true,
           autoFocus: true,
@@ -86,11 +121,17 @@ angular.module('checkmate.DeploymentData')
 
         $scope.$on('deployment:update', function(event, data) {
           if(!$scope.codemirror.isFocused) {
-            var yamlData = jsyaml.safeDump(data);
+            var newDeployment;
 
-            if ($scope.deployment != yamlData) {
+            if($scope.codemirror.options.mode == 'application/json') {
+              newDeployment = JSON.stringify(data, undefined, 2);
+            } else {
+              newDeployment = jsyaml.safeDump(data);
+            }
+
+            if ($scope.deployment != newDeployment) {
               $timeout(function() {
-                $scope.deployment = yamlData;
+                $scope.deployment = newDeployment;
               });
             }
           }
