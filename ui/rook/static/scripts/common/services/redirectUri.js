b3d5c0497429c7c3b4201f4634b55c1fa17ac764
Chris Burrell <chrisburrell@gmail.com>
2015-03-13 15:01:19 -0500

    fix(checkmate.redirectUri): remove whitelist filter

--- ui/rook/static/scripts/common/services/redirectUri.js
+++ ui/rook/static/scripts/common/services/redirectUri.js
@@ -4,22 +4,17 @@ angular.module('checkmate.redirectUri')
   .factory('redirectUri', function($routeParams, $window) {
     'use strict';
 
-    var allowedUris = ['waldo.rax.io'];
-    var uri = $routeParams.redirect_uri;
-
     return redirect;
 
     /**
-     * Searches for white-listed URIs and will consume redirect_uri.
+     * Uses redirect_uri param for redirecting.
      * @return {} A redirect or nothing.
      */
     function redirect() {
+      var uri = $routeParams.redirect_uri;
+
       if(uri) {
-        _.each(allowedUris, function(allowedUri) {
-          if(uri.indexOf(allowedUri) > -1) {
-            return $window.location.href = uri;
-          }
-        }); 
+        return $window.location.href = uri;
       }
 
       return;
