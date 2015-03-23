1bd76b5cb9b3d61b6eb45ca88c51d1d880c19495
Hart Hoover <hart.hoover@rackspace.com>
2015-03-23 15:13:27 -0500

    feat(docker) Add crypt to Dockerfile

--- docker/config/run.sh
+++ docker/config/run.sh
@@ -11,4 +11,13 @@ tail -f /var/log/checkmate-stdout.log&
 
 echo "Starting Supervisor..."
 
-exec supervisord -n -c /etc/supervisord.conf
+SUPERVISOR_CMD="supervisord -n -c /etc/supervisord.conf"
+
+if [ $LOAD_ENV_FROM_ETCD ]
+then
+    exec /app/docker/config/envfromjson.py \
+    -f <(crypt get -endpoint=$ETCDCTL_PEERS -secret-keyring="/etc/configs/secring.gpg" /app/checkmate/$PREFIX/environment) -- \
+    $SUPERVISOR_CMD
+else
+    exec $SUPERVISOR_CMD
+fi
