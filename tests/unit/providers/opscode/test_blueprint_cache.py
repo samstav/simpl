daaceb4cf13c675464063245ae610fee6d3dedb7
Sam Stavinoha <smlstvnh@gmail.com>
2015-04-27 20:06:40 -0500

    more comprehensive testing for BlueprintCache
    Tests the following scenarios:
    
        - a branch ref is used in repo url gh.com/usr/prj#branch
        - a tag ref is used in repo url gh.com/usr/prj#tag
        - a commit ref is used in repo url gh.com/usr/prj#<commmit_hash>
        - a short commit ref is used in repo url gh.com/usr/prj#<short_commit_hash>
        - a tag ref takes precedence over a branch name if they are the same
        - the revision is validated on every BlueprintCache usage using
          the ls-remote command
        - the latest revisions are only fetched if necessary
        - if a repo has previously been cloned:
            - the clone's revision for a branch is updated only if
              the corresponding remote branch has changed
            - the clone's revision for a tag is updated only if the
              tag has moved on the remote
        - the operation fails if:
            - a bad (nonexistent or invalid) ref is used
              (raises InvalidRepoUrl)
            - a bad (nonexistent or invalid) repo location is used
              (raises InvalidRepoUrl)

--- tests/unit/providers/opscode/test_blueprint_cache.py
+++ tests/unit/providers/opscode/test_blueprint_cache.py
@@ -16,8 +16,11 @@
 
 import errno
 import os
+import shutil
 import tempfile
 
+import mock
+import simpl
 import unittest
 
 from checkmate import exceptions as cmexc
@@ -31,23 +34,19 @@ class TestBlueprintCache(unittest.TestCase):
     repo_cache_base = os.path.join(
         tempfile.gettempdir(), 'checkmate-test-blueprint-cache')
 
-    @classmethod
-    def setUpClass(cls):
-        cls.patch_repo_cache_base()
+    def setUp(self):
+        self.repo_cache_base_patcher = mock.patch.object(
+            bpc_mod, 'repo_cache_base')
+        repo_cache_base_mock = self.repo_cache_base_patcher.start()
+        repo_cache_base_mock.return_value = self.repo_cache_base
 
-    @classmethod
-    def tearDownClass(cls):
+    def tearDown(self):
+        self.repo_cache_base_patcher.stop()
         try:
-            shutil.rmtree(cls.repo_cache_base)
+            shutil.rmtree(self.repo_cache_base)
         except OSError as err:
             if err.errno != errno.ENOENT:
                 raise
-
-    @classmethod
-    def patch_repo_cache_base(self):
-        # patch repo_cache_base to avoid
-        # OSError: [Errno 13] Permission denied: '/var/local'
-        bpc_mod.repo_cache_base = lambda: self.repo_cache_base
 
 
 class TestUpdateBPC(TestBlueprintCache):
@@ -102,7 +101,163 @@ class TestUpdateBPC(TestBlueprintCache):
             tdc.commit(target_dir_path)  # Should not fail
 
 
+TEST_GIT_USERNAME = 'checkmate_blueprint_cache_test_user'
+
+
+def _configure_test_user(gitrepo):
+
+    email = '%s@%s.test' % (TEST_GIT_USERNAME, TEST_GIT_USERNAME)
+    gitrepo.run_command('git config --local user.name %s' % TEST_GIT_USERNAME)
+    gitrepo.run_command('git config --local user.email %s' % email)
+
+
+class TestBPCRefs(TestBlueprintCache):
+
+    def setUp(self):
+        super(TestBPCRefs, self).setUp()
+        self.remote = simpl.git.GitRepo.init(temp=True)
+        _configure_test_user(self.remote)
+        self.remote.commit(message='Initial commit')
+        self.initial_revision = self.remote.head
+
+    def test_defaults_to_master_ref(self):
+        source_repo = self.remote.repo_dir
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        self.assertTrue(bpc.source_ref == 'master')
+
+    def test_ref_can_be_tag(self):
+        self.remote.commit(message='tag this commit')
+        tag = 'whatatag'
+        tagged_revision = self.remote.head
+        self.remote.tag(tag)
+        self.remote.run_command(
+            ['git', 'reset', '--hard', self.initial_revision])
+        source_repo = '%s#%s' % (self.remote.repo_dir, tag)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        # this assertion is to prove we aren't "just getting lucky"
+        self.assertNotEqual(bpc.repo.head, self.remote.head)
+        self.assertEqual(bpc.repo.head, tagged_revision)
+
+    def test_ref_can_be_branch(self):
+        self.remote.commit(message='branch from this commit')
+        branch = 'whatabranch'
+        branched_revision = self.remote.head
+        self.remote.branch(branch)
+        self.remote.run_command(
+            ['git', 'reset', '--hard', self.initial_revision])
+        source_repo = '%s#%s' % (self.remote.repo_dir, branch)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        # this assertion is to prove we aren't "just getting lucky"
+        self.assertNotEqual(bpc.repo.head, self.remote.head)
+        self.assertEqual(bpc.repo.head, branched_revision)
+
+    def test_ref_can_be_commit_hash(self):
+        self.remote.commit(message='reference this commit')
+        committed_revision = self.remote.head
+        self.remote.run_command(
+            ['git', 'reset', '--hard', self.initial_revision])
+        source_repo = '%s#%s' % (self.remote.repo_dir, committed_revision)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        # this assertion is to prove we aren't "just getting lucky"
+        self.assertNotEqual(bpc.repo.head, self.remote.head)
+        self.assertEqual(bpc.repo.head, committed_revision)
+
+    def test_ref_can_be_short_commit_hash(self):
+        self.remote.commit(message='reference this commit')
+        short_committed_revision = self.remote.head[:8]
+        self.remote.run_command(
+            ['git', 'reset', '--hard', self.initial_revision])
+        source_repo = '%s#%s' % (
+            self.remote.repo_dir, short_committed_revision)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        # this assertion is to prove we aren't "just getting lucky"
+        self.assertNotEqual(bpc.repo.head, self.remote.head)
+        self.assertTrue(bpc.repo.head.startswith(short_committed_revision))
+
+    def test_tag_ref_gets_updated_on_remote(self):
+        tag = 'whatatag'
+        self.remote.tag(tag)
+        source_repo = '%s#%s' % (self.remote.repo_dir, tag)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        self.remote.commit(message='re-tag this commit')
+        desired_revision = self.remote.head
+        self.remote.tag(tag)
+        # assert that we *need* to update
+        self.assertNotEqual(bpc.repo.head, desired_revision)
+        bpc.update()
+        # assert that calling update() fixed our clone
+        self.assertEqual(bpc.repo.head, desired_revision)
+
+    def test_branch_ref_gets_updated_on_remote(self):
+        branch = 'whatabranch'
+        self.remote.branch(branch)
+        source_repo = '%s#%s' % (self.remote.repo_dir, branch)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        self.remote.commit(message='re-branch this commit')
+        desired_revision = self.remote.head
+        # this will overwrite the 'whatabranch' branch
+        self.remote.branch(branch)
+        # assert that we *need* to update
+        self.assertNotEqual(bpc.repo.head, desired_revision)
+        bpc.update()
+        # assert that calling update() fixed our clone
+        self.assertEqual(bpc.repo.head, desired_revision)
+
+    def test_bad_person_uses_same_name_gets_tag(self):
+        """Prefer tags to branches."""
+        tag_and_branch = 'spam'
+        self.remote.commit(message='tag me')
+        self.remote.tag(tag_and_branch)
+        tagged_revision = self.remote.head
+        self.remote.commit(message='branch me')
+        self.remote.branch(tag_and_branch)
+        source_repo = '%s#%s' % (self.remote.repo_dir, tag_and_branch)
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        self.assertEqual(bpc.repo.head, tagged_revision)
+
+    def test_no_such_ref(self):
+        self.remote.tag('realness')
+        source_repo = '%s#%s' % (self.remote.repo_dir, 'wat')
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        with self.assertRaises(cmexc.CheckmateInvalidRepoUrl) as cntxt:
+            bpc.update()
+        friendly = ("Invalid ref 'wat' for repo. The ref must be a tag, "
+                    "branch, or commit hash known to %s."
+                    % self.remote.repo_dir)
+        self.assertEqual(friendly, cntxt.exception.friendly_message)
+
+    def test_no_such_repo(self):
+        source_repo = '%s' % 'i/dont/exist'
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        with self.assertRaises(cmexc.CheckmateInvalidRepoUrl) as cntxt:
+            bpc.update()
+        friendly = "Git repository could not be cloned from 'i/dont/exist'."
+        self.assertEqual(friendly, cntxt.exception.friendly_message)
+
+    def test_has_been_deleted_or_made_private_since_clone(self):
+        source_repo = self.remote.repo_dir
+        bpc = bpc_mod.BlueprintCache(source_repo)
+        bpc.update()
+        # this should be equivalent to a repo going private
+        shutil.rmtree(self.remote.repo_dir)
+        with self.assertRaises(cmexc.CheckmateInvalidRepoUrl) as cntxt:
+            bpc.update()
+        friendly = ('Could not access a repo previously cloned from %s'
+                    % self.remote.repo_dir)
+        self.assertEqual(friendly, cntxt.exception.friendly_message)
+
+
 if __name__ == '__main__':
+
     import sys
+
     from checkmate import test as cmtest
+
     cmtest.run_with_params(sys.argv[:])
